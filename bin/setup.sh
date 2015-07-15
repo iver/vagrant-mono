8f0de3d8cadad4c1455cf91abb565f4d93d72c97
Ivan Jaimes <ivan.jaimes@iver.mx>
2015-07-15 14:05:32 -0500

    Setup script updated

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,7 +2,6 @@
 apt-get update
 
 apt-get install -y mono-complete monodevelop mono-addins-utils gtk-sharp2 gnome-sharp2 autoconf automake git
-
 mozroots --import --sync
 
 yes | certmgr -ssl https://go.microsoft.com
@@ -18,17 +17,12 @@ yes | certmgr -ssl -m https://nuget.org
 yes | certmgr -ssl -m https://www.myget.org/F/aspnetvnext/
 
 git clone https://github.com/mono/monodevelop.git /home/vagrant/monodevelop
-
 cd /home/vagrant/monodevelop
-
 git submodule update --init --recursive
-
-./configure --profile=stable ; make 
-
+./configure --profile=stable ; make
 make install
 
 mkdir /tmp/nuget
-
 sudo chown vagrant /tmp/nuget
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -36,5 +30,6 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 apt-get update
-
 apt-get -y install google-chrome-stable
+apt-get -y install xfce4 xfce4-goodies xdm
+
